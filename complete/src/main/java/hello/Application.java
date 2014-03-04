905fe19f6ea59c9884744f95fc26390813008810
Greg Turnquist <gturnquist@gopivotal.com>
2014-03-04 14:41:49 -0500

    Upgrade to Spring Boot 1.0.0.RC4

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -5,36 +5,11 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-    
-    @Bean
-    ServletContextTemplateResolver templateResolver() {
-        ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
-        resolver.setSuffix(".html");
-        resolver.setTemplateMode("HTML5");
-        return resolver;
-    }
-    
-    @Bean
-    SpringTemplateEngine engine() {
-        SpringTemplateEngine engine = new SpringTemplateEngine();
-        engine.setTemplateResolver(templateResolver());
-        return engine;
-    }
-    
-    @Bean
-    ThymeleafViewResolver viewResolver() {
-        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
-        viewResolver.setTemplateEngine(engine());
-        return viewResolver;
-    }
     
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
--- complete/src/main/java/hello/WebController.java
+++ complete/src/main/java/hello/WebController.java
@@ -7,9 +7,17 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 
 @Controller
-public class WebController {
+public class WebController extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
 
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String showForm(Person person) {
@@ -21,7 +29,7 @@ public class WebController {
         if (bindingResult.hasErrors()) {
             return "form";
         }
-        return "redirect:/results.html";
+        return "redirect:/results";
     }
 
 }
