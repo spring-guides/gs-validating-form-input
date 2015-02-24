7f39724183bd77eabd228c52052f0a45857a1572
Greg Turnquist <gturnquist@gopivotal.com>
2015-02-24 10:32:11 -0600

    Convert to @SpringBootApplication

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,16 +1,11 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
-    
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
