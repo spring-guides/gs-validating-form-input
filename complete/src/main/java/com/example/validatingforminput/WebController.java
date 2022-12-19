7bc241e87436fa38cc49b2f7d05ce8f32727fad0
Jay Bryant <jbryant@vmware.com>
2022-12-19 14:23:34 -0600

    Upgraded to Spring Boot 3.0.0
    to get onto the new major version of Spring Boot.

--- complete/src/main/java/com/example/validatingforminput/WebController.java
+++ complete/src/main/java/com/example/validatingforminput/WebController.java
@@ -1,6 +1,6 @@
 package com.example.validatingforminput;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
